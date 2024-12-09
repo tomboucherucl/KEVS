1443bd288c6ca64810812b29dcefeec11a5457cd
Thomas Boucher <rmaptbo@ucl.ac.uk>
2024-12-09 12:12:37 +0000

    Update to structure

---
+++ scripts/main.py
@@ -0,0 +1,7 @@
+# Code to process images, make U-Mamba predictions, and then create KEVS based predictions
+
+### (1) resample CT images to (1.5,1.5.1.5) ###
+
+### (2) use U-Mamba to make predictions ###
+
+### (3) make VAT prediction using KEVS
