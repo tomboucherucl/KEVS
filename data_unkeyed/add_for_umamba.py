822adde030e343d5af40c998c23300367bdbee37
Thomas Boucher <rmaptbo@ucl.ac.uk>
2025-01-09 12:17:59 +0000

    update

--- data_unkeyed/add_for_umamba.py
+++
@@ -1,9 +0,0 @@
-import os
-
-sorted_list = sorted(os.listdir('niftis_new_batch'))
-
-for f in sorted_list:
-    new_name = f.replace('.nii.gz', '_0000.nii.gz')
-    print(new_name)
-    os.rename(f'niftis_new_batch/{f}', f'niftis_new_batch/{new_name}')
-    print(f"Renamed niftis_new_batch/{f} to niftis_new_batch/{new_name}")
--- data_unkeyed/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-nifti_dir = 'filtered_preds'
-
-for file in os.listdir(nifti_dir):
-    first_part = file[:26]
-    #print(f"{first_part}_0000.nii.gz")
-    os.rename(os.path.join(nifti_dir, file), os.path.join(nifti_dir, f"{first_part}.nii.gz"))
